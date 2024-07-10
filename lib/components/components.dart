fa0fd27ee6cf9252e52dc285356f735fd0768a05
A. K. M. Mahbub Ullah <48197772+Shishir335@users.noreply.github.com>
2024-07-10 11:14:05 +0600

    init

---
+++ lib/components/components.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget horizontalGap([double width = 20]) {
+  return SizedBox(width: width);
+}
+
+Widget verticalGap([double height = 20]) {
+  return SizedBox(height: height);
+}
---
+++ lib/utils.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppConfigurations {
+  // colors
+  static Color backgroundColor = const Color(0xff16131e);
+  static Color foregroundColor = Colors.white;
+  static Color hint = Colors.grey;
+
+  // textStyles
+  static TextStyle title = const TextStyle(
+      color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold);
+  static TextStyle subtitle = const TextStyle(
+      color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold);
+  static TextStyle bodyText =
+      const TextStyle(color: Colors.white, fontSize: 14);
+  static TextStyle headline = const TextStyle(
+      color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold);
+  static TextStyle hintText = const TextStyle(
+      color: Colors.grey, fontSize: 16, fontWeight: FontWeight.bold);
+}
